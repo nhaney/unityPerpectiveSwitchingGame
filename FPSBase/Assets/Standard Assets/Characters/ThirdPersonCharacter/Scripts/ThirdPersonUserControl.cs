e80c70c29f8f5761f7c1e2e187ed7acfc525d967
nhaney <nhaney@eecs.wsu.edu>
2018-05-01 22:52:25 -0700

    importing base fps stuff
    base game

---
+++ FPSBase/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof (ThirdPersonCharacter))]
+    public class ThirdPersonUserControl : MonoBehaviour
+    {
+        private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
+        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+        private Vector3 m_CamForward;             // The current forward direction of the camera
+        private Vector3 m_Move;
+        private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+
+        
+        private void Start()
+        {
+
+        }
+
+
+        private void Update()
+        {
+
+        }
+
+
+        // Fixed update is called in sync with physics
+        private void FixedUpdate()
+        {
+
+        }
+    }
+}
---
+++ FPSBase/Assets/cameraSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class cameraSwitcher : MonoBehaviour {
+
+    public Camera fpCamera;
+    public Camera thisCam;
+
+	// Use this for initialization
+	void Start () {
+        fpCamera.enabled = true;
+        thisCam.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            fpCamera.enabled = !fpCamera.enabled;
+            thisCam.enabled = !thisCam.enabled;
+            gameObject.GetComponentInParent<FirstPersonController>().isSideView = !gameObject.GetComponentInParent<FirstPersonController>().isSideView;
+        }
+	}
+}
